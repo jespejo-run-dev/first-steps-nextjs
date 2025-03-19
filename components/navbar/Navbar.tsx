bde3cd18e2852e67e07a42a3df9800b8522eb489
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-19 13:35:17 -0300

    Server Components - Async await

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
 
-export const Navbar = () => {
+const temporalAsync = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true)
+        }, 2000)
+    })
+}
 
-    console.log('Navbar creado');
-    
+export const Navbar = async() => {
+
+    await temporalAsync();
+
   return (
     <nav className="flex bg-[rgba(29,78,216,0.3)] p-2 m-2 rounded">
         <span>Home</span>
